f3caed40d5746058bc36001ac1cb6ba3e3fe1784
Sergey Zhukov <zhus@mail.ru>
2023-06-16 12:41:49 +0300

    restored name of exe

--- compile.cmd
+++ compile.cmd
@@ -1,2 +1,2 @@
 @call "c:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
-@native-image -jar target\checkPdfContainsEOF-jar-with-dependencies.jar -o pdfContainsEOF
+@native-image -jar target\checkPdfContainsEOF-jar-with-dependencies.jar -o checkPdfContainsEOF
