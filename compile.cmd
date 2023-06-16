6fa97de6dac3ff32fdd173cc902b3a9c114901ee
Sergey Zhukov <zhus@mail.ru>
2023-06-16 12:56:39 +0300

    maven jar assembly

--- compile.cmd
+++ compile.cmd
@@ -1,2 +1,2 @@
 @call "c:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
-@native-image -jar target\checkPdfContainsEOF-jar-with-dependencies.jar -o checkPdfContainsEOF
+@native-image -jar target\checkPdfContainsEOF-1.1.jar -o checkPdfContainsEOF
